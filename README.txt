495afe515d445ea49235f2d40bea31ac237f08ac
Douglas Burke <dburke@cfa.harvard.edu>
2014-12-05 10:27:47 -0500

    Import of version 0.1a

---
+++ README.txt
@@ -0,0 +1,113 @@
+
+ISMabs:
+
+ISMabs is an X-ray photoabsorption model for the interstellar
+medium that takes into account both neutral and ionized 
+species from H, He, N, O, Ne, Mg, Si, S, Ar, Ca, Fe, Ni and Zn.  
+Particularly we include in our model the following cross 
+sections (See Gatuzz et al. (2014b) for details.)
+
+- Neutral states of Si, S, Ar and Ca from Verner et al.
+(1995)
+- Singly and doubly ionized states of Si, S, Ar and Ca
+from Witthoeft et al. (2009) and Witthoeft et al. (2011)
+- Neutral, singly and doubly ionized states of N from
+Garcia et al. (2009)
+- Neutral states of O from Gorczyca et al. (2013)
+- Singly and doubly ionized states of O from Garcia et al.
+(2005), including corrections applied by Gatuzz et al.
+(2013)
+- Neutral state of Ne from Gorczyca et al. (2000)
+- Singly and doubly ionized states of Ne from Gorczyca et
+al. (2005)
+- For the Fe-L edge region we use the measurement of
+metallic iron by Kortright & Kim (2000)
+- Neutral, singly and doubly ionized states of Mg from
+Hasoglu et al. (2014).
+
+Compared with previous photoabsorption models, which solely 
+rely on neutral species, the inclusion of ions leads to 
+improvements of the spectral fits. Fit parameters comprise 
+the column densities of abundant contributors that allow 
+direct estimates of ionization states. 
+
+OBTAINING ISMabs
+
+As of this writing, the ISMabs package is not included as 
+part of the standard xspec software, either within heasoft 
+or as part of standalone xspec.  Instead, it must be downloaded 
+and installed separately from the sites:
+
+http://hea-www.cfa.harvard.edu/~javier/ismabs/index.html
+
+The contents of the tarfile include:
+
+atomic_data/AtomicData.fits  -- atomic database binary fits file.
+This must reside in the directory atomic_data inside the folder 
+where the model is located.  
+
+ismabs.f90 -- source code for ISMabs
+
+lmodel.dat -- local model definition file needed by xspec.  
+
+compile.sh -- installation script written on bash.
+
+README.txt -- this file
+
+INSTALLATION
+
+You can use the compile.sh file to install the model by doing
+
+sh compile.sh
+
+In the  model folder or you can setting up and using this 
+model is as described in the xspec manual:
+
+
+0) You need to have the heasoft package installed on your 
+machine, but it must be built from source.  Local models 
+cannot be installed from a binary installation.
+
+1) untar this directory somewhere in your user area
+
+2) setup your headas environment (eg. 'setenv HEADAS /path/to/architecture',
+and 'source \$HEADAS/headas-init.csh')
+
+
+3) start up xspec, and in response to the prompt type 
+
+'initpackage xstarmod lmodel.dat <path-to-current-directory>',
+
+where <path-to-current-directory> is the full path of the current directory 
+
+
+
+After the build is complete type 
+
+'lmod ismabs <path-to-current-directory>'
+
+In subsequent  sessions you don't neet to do the initpackage 
+step again, just the lmod.
+
+PARAMETERS
+
+Inside of xspec, the model can be invoked by typing 
+'mo ismabs*pow' or variations on that.
+The input parameters included the elemental column 
+densities and redshift.
+
+NOTE that the HeI column density is not a free parameter 
+due to the constraining imposed to the model:
+
+HeI = 0.1 * H
+
+See Gatuzz et al. (2014b) for details.
+
+CONTACT
+
+This package is still being tested. Please contact me with 
+any reports or questions.
+
+egatuzz@ivic.gob.ve
+
+
---
+++ ismabs.f90
@@ -0,0 +1,367 @@
+! +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+!  ISMABS
+!  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+!  XSPEC local model for ISM absorption
+!  Version 1.0 June 2014
+!  (Without Ni and Zn in this version)
+!
+! To-Do:
+! - Add turbulence
+! - Add molecular-solid cross sections
+! +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+subroutine ismabs(ear, ne, param, ifl, photar)
+!
+! The main routine to call all subroutines
+!
+
+ implicit none
+ integer,parameter :: num_param = 31, out_unit=20, nion=30 
+ integer,parameter :: nemod=650000  !Number of elements for each ion cross section.  
+ integer :: ne, ifl, a
+ double precision :: nH, rshift, emod(1:nemod), coemod(nemod), bxs(0:nion,nemod), cion(nion), bener(1:nemod)
+ double precision :: N_He_0, N_He_1, N_C_0, N_C_1, N_C_2
+ double precision :: N_N_0, N_N_1, N_N_2, N_O_0, N_O_1, N_O_2
+ double precision :: N_Ne_0, N_Ne_1, N_Ne_2, N_Mg_0, N_Mg_1, N_Mg_2
+ double precision :: N_Si_0, N_Si_1, N_Si_2, N_S_0, N_S_1, N_S_2
+ double precision :: N_Ar_0, N_Ar_1, N_Ar_2, N_Ca_0
+ double precision :: N_Ca_1, N_Ca_2, N_Fe_0
+ double precision :: zfac  
+ real :: ear(0:ne), param(num_param),photar(ne)
+ logical :: startup=.true.
+ character (len=40) version
+ version='0.1a'
+
+ if(startup)then
+  print *, ' '
+  print *, 'ISMabs: ISM absorption model Version',version
+  print *, 'Gatuzz, Garcia, Kallman, Mendoza, & Gorczyca (2014)'
+  print *, 'Note: Default column densities are given'
+  print *, 'according to Grevesse, N. & Sauval (1998)'     
+  print *, 'assuming N_H = 1.E21 cm^-2'  
+  print *, ' '
+  call read_cross_sections( nemod,bxs,ifl)
+  call create_energy_grid(1.d1,1.d4,bener,nemod) !Absorption coefficient calculation grid  = cross section grid
+  startup=.false.  
+ endif
+ 
+! Model parameters
+ nH  = param(1)
+ N_He_0 = 0.1*nH
+ N_He_1 = param(2)
+ N_C_0 = param(3)
+ N_C_1 = param(4)
+ N_C_2 = param(5)
+ N_N_0 = param(6)
+ N_N_1 = param(7)
+ N_N_2 = param(8)
+ N_O_0 = param(9)
+ N_O_1 = param(10)
+ N_O_2 = param(11)
+ N_Ne_0 = param(12)
+ N_Ne_1 = param(13)
+ N_Ne_2 = param(14)  
+ N_Mg_0 = param(15)
+ N_Mg_1 = param(16)
+ N_Mg_2 = param(17)    
+ N_Si_0 = param(18)
+ N_Si_1 = param(19)
+ N_Si_2 = param(20)    
+ N_S_0 = param(21)
+ N_S_1 = param(22)
+ N_S_2 = param(23)   
+ N_Ar_0 = param(24)
+ N_Ar_1 = param(25)
+ N_Ar_2 = param(26)
+ N_Ca_0 = param(27)
+ N_Ca_1 = param(28)
+ N_Ca_2 = param(29)
+ N_Fe_0 = param(30)
+ rshift = param(31)
+ zfac = 1/(1.d0+dble(rshift))
+
+
+
+
+! To read the cross-sections once for each dataset
+ if(a.lt.ifl)then
+  a= a+1
+  print *, 'Loading atomic-data for dataset',ifl
+  call read_cross_sections( nemod,bxs,ifl)
+  call create_energy_grid(1.d1,1.d4,bener,nemod) !Absorption coefficient calculation grid  = cross section grid
+ endif
+
+
+ 
+!
+ call absorption(nH, N_He_0, N_He_1, N_C_0, N_C_1, N_C_2, &
+ N_N_0, N_N_1, N_N_2, N_O_0, N_O_1, N_O_2, &
+ N_Ne_0, N_Ne_1, N_Ne_2, N_Mg_0, N_Mg_1, N_Mg_2, &
+ N_Si_0, N_Si_1, N_Si_2, N_S_0, N_S_1, N_S_2, &
+ N_Ar_0, N_Ar_1, N_Ar_2, N_Ca_0, &
+ N_Ca_1, N_Ca_2, N_Fe_0,  &
+ zfac, emod, nemod, coemod,bxs,cion,ifl,bener)
+
+!
+ call map_to_grid(dble(ear),ne,emod,nemod,photar,coemod,ifl)
+
+
+ return
+ end subroutine ismabs    
+
+
+! +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+subroutine read_cross_sections(bnene,xs,ifl)
+!
+!  This routine reads all cross sections and puts them on a given grid
+!
+   implicit none
+
+   integer,parameter :: nion=30, out_unit=20
+   integer :: bnene, ifl, i, j, status
+   integer :: nemax(0:nion)
+   double precision :: ener(0:nion,bnene), xs(0:nion,bnene)
+   character (len=240) :: filename2
+   character (len=240) :: local_dir = '/Users/javier/local-xspec-models/ismabs/last/Ismabs'
+   integer inunit,readwrite,blocksize
+   integer :: hdutype,colnum
+   integer :: felem=1, nulld=0
+   logical :: anynull
+
+!Number of elements for each ion cross section.  
+   do i=0,nion
+      nemax(i)=650000
+   enddo
+    
+! parameters to specify the opening process  
+  status=0
+  readwrite=0
+  blocksize=1
+  filename2=trim(local_dir)//'/atomic_data/AtomicData.fits'
+
+! Get an unused Logical Unit Number to use to open the FITS file.
+  call ftgiou(inunit,status)
+  
+! Open the FITS file
+  call ftopen(inunit,filename2,readwrite,blocksize,status)
+ 
+! Move to the extension 2 (the binary table)
+  call ftmahd(inunit,2,hdutype,status)
+
+!Read one energy grid (column 1)
+ do i=0, 1
+  do j=1,nemax(i)
+   colnum=1
+   call ftgcvd(inunit,colnum,j,felem,1,nulld,ener(i,j),anynull,status)
+  enddo
+ enddo
+
+!Assign the energy grid to all ion energy grids
+ do i=1, nion
+  do j=1,nemax(i)
+   ener(i,j)= ener(0,j)
+  enddo
+ enddo
+
+!Read cross sections 
+ colnum=2
+ do i=0, nion
+  do j=1,nemax(i)
+   call ftgcvd(inunit,colnum,j,felem,1,nulld,xs(i,j),anynull,status)
+  enddo
+  colnum=colnum+1
+ enddo
+
+!  Close the file and free the unit number
+ call ftclos(inunit, status)
+ call ftfiou(-1, status)
+
+end subroutine read_cross_sections
+
+! ======================================= !
+ subroutine absorption(col22, N_He_0, N_He_1, N_C_0, N_C_1, N_C_2, &
+ N_N_0, N_N_1, N_N_2, N_O_0, N_O_1, N_O_2, &
+ N_Ne_0, N_Ne_1, N_Ne_2, N_Mg_0, N_Mg_1, N_Mg_2, &
+ N_Si_0, N_Si_1, N_Si_2, N_S_0, N_S_1, N_S_2, &
+ N_Ar_0, N_Ar_1, N_Ar_2, N_Ca_0, &
+ N_Ca_1, N_Ca_2, N_Fe_0,    &
+ zfac, e1, bnene, coeff, bxs2,cion,ifl,bener)
+!
+!  This is routine that calculates the optical depth given the column densities
+!  Finally returns the absorption coefficient exp(-tau)
+!
+ implicit none
+
+ integer,parameter :: nion=30, out_unit=20
+ integer :: bnene,  ifl
+ integer :: i, j
+ double precision :: col22, col, tmp, cion(nion)
+ double precision :: bener(0:bnene), bxs2(0:nion,bnene), e1(0:bnene)
+ double precision :: tau, coeff(bnene)
+ double precision :: N_He_0, N_He_1, N_C_0, N_C_1, N_C_2
+ double precision :: N_N_0, N_N_1, N_N_2, N_O_0, N_O_1, N_O_2
+ double precision :: N_Ne_0, N_Ne_1, N_Ne_2, N_Mg_0, N_Mg_1, N_Mg_2
+ double precision :: N_Si_0, N_Si_1, N_Si_2, N_S_0, N_S_1, N_S_2
+ double precision :: N_Ar_0, N_Ar_1, N_Ar_2, N_Ca_0
+ double precision :: N_Ca_1, N_Ca_2, N_Fe_0
+ double precision :: zfac 
+ real hphoto, gphoto
+ external hphoto, gphoto
+      
+!Column densities
+ cion(1)=N_He_0
+ cion(2)=N_He_1
+ cion(3)=N_C_0
+ cion(4)=N_C_1
+ cion(5)=N_C_2
+ cion(6)=N_N_0
+ cion(7)=N_N_1
+ cion(8)=N_N_2
+ cion(9)=N_O_0
+ cion(10)=N_O_1
+ cion(11)=N_O_2
+ cion(12)=N_Ne_0
+ cion(13)=N_Ne_1
+ cion(14)=N_Ne_2
+ cion(15)=N_Mg_0
+ cion(16)=N_Mg_1
+ cion(17)=N_Mg_2
+ cion(18)=N_Si_0
+ cion(19)=N_Si_1
+ cion(20)=N_Si_2
+ cion(21)=N_S_0
+ cion(22)=N_S_1
+ cion(23)=N_S_2
+ cion(24)=N_Ar_0
+ cion(25)=N_Ar_1
+ cion(26)=N_Ar_2
+ cion(27)=N_Ca_0
+ cion(28)=N_Ca_1
+ cion(29)=N_Ca_2
+ cion(30)=N_Fe_0
+
+  
+       
+!  Calculates the optical depth and the absorption coefficient exp(-tau)
+ col=col22*1.d22
+ e1(0)=(bener(0)*zfac)/1.d3
+
+ 
+ do i=1,bnene
+  e1(i)=(bener(i)*zfac)/1.d3
+
+! In case you want to read xspec hydrogen column density
+!      bxs2(0,i)=hphoto(real(e1(i-1)),real(e1(i)))   
+
+! tau for hydrogen column density 
+  tmp=col*bxs2(0,i)
+
+!  Calculates the optical depth and the absorption coefficient exp(-tau)
+
+  tmp= tmp+(bxs2(1,i)*0.1*col) ! He I column density = 0.1 Nh      
+
+  do j=2,nion
+   tmp=tmp+(cion(j)*bxs2(j,i)*1.d16)
+  enddo
+
+  tau=tmp
+  coeff(i)=dexp(-tau)
+     
+ enddo
+
+
+ end subroutine absorption
+! ======================================= !
+
+ subroutine map_to_grid(new_en,nne,old_en, one,   nflux, old_flu,ifl)
+ ! This routine map to a given grid
+ implicit none
+ integer :: i, j, k, one, nne, bmin, bmax,ifl
+ double precision :: new_en(0:nne)
+ double precision :: old_en(0:one), old_flu(one)
+ double precision :: stemp,etemp, s, etemp2
+ real :: nflux(nne)
+ integer,parameter :: out_unit=20
+
+ do i=1,nne
+  nflux(i)=real(0.d0)
+  call dbinsrch(new_en(i-1),bmin,old_en,one+1)
+  call dbinsrch(new_en(i),bmax,old_en,one+1)
+  bmin = bmin-1
+  bmax = bmax-1
+
+  ! Linear interpolation
+  if (bmin.eq.bmax) then
+   if(new_en(i).le.old_en(1))then
+    s=real(old_flu(1))
+   else if(new_en(i).gt.old_en(one))then
+    s=real(old_flu(one))
+   else
+    do j=2,one
+     if(new_en(i).gt.old_en(j-1).and.new_en(i).le.old_en(j))then
+      etemp2=(new_en(i)+new_en(i-1))/2
+      s=old_flu(j-1)+(old_flu(j)-old_flu(j-1))*(etemp2-old_en(j-1))/(old_en(j)-old_en(j-1))
+     endif
+    enddo
+   endif
+
+  ! Average (integral)
+   else
+    stemp=0.d0
+    etemp=0.d0  
+    do k=bmin,bmax
+     stemp=stemp+(old_flu(k))*(old_en(k)-old_en(k-1))
+     etemp=etemp+(old_en(k)-old_en(k-1))
+    enddo
+     s=real(stemp/etemp)
+    endif
+
+    nflux(i)=real(s)
+  enddo
+
+ end subroutine map_to_grid
+
+! ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+ subroutine dbinsrch(e,k,ener,n)
+  !
+  !     search for energy e in array ener(1:n) and return bin k for
+  !     which ener(k).le.e.lt.ener(k+1)
+  !     adapted from J. Wilms's tbvabs_new.f routine
+  !
+ implicit none
+ double precision :: e
+ integer :: n,k,klo,khi
+ double precision :: ener(n)
+
+
+  klo=1
+  khi=n
+1 if (khi-klo.gt.1) then
+   k=(khi+klo)/2
+   if(ener(k).gt.e)then
+    khi=k
+   else
+    klo=k
+   endif
+   goto 1
+  endif
+ 
+  if (klo.eq.0) then
+   print *,'Energy out of bounds. Should not happen'
+   stop
+  endif
+  k=klo
+
+ end subroutine dbinsrch
+
+! ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+ subroutine create_energy_grid(emin,emax,en,nen)
+ implicit none
+ integer :: i, nen
+ double precision :: en(nen)
+ double precision  :: emin,emax
+! 
+ do i=1,nen
+  en(i)=10**(((log10(emax)-log10(emin))*real(i)/nen)+log10(emin))
+ enddo
+!
+ end subroutine create_energy_grid
